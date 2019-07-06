82cf1df68901de4e5e3aa2f1b6d310d388593794
seimangorira <seimangorira@gmail.com>
2019-07-06 18:41:00 +0900

    update

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -3,7 +3,11 @@ $(function() {
     $(this).delay(2000).fadeOut(2000, function() {
       $(".message-2").fadeIn(1000, function(){
         $(this).delay(3000).fadeOut(2000,function(){
-
+          $(".message-3").fadeIn(1000, function(){
+            $(this).delay(3000).fadeOut(2000, function(){
+              $(".message-4").fadeIn(2000)
+            });
+          });
         });
       });
     });
