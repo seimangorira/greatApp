24083edd651fc51d7b5b3d85240aad1331207244
seimangorira <seimangorira@gmail.com>
2019-07-06 18:58:39 +0900

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_action :move_to_index, except: :top
   def top
+  end
+
+  def index
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :top unless user_signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "/" => "home#index"
   get 'home/top' => "home#top"
 end
